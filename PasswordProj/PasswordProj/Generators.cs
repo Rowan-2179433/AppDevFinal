5fe32b2a4194b3b106f24c5f5c15c055d1f93a9f
Rowan <112976591+Rowan-2179433@users.noreply.github.com>
2023-12-02 05:17:40 -0500

    First iteration of the proj
    Let's gooooooo

---
+++ PasswordProj/PasswordProj/Generators.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordProj
+{
+    internal class Generators
+    {
+
+        public string generatePassword(int length, bool numbers, bool specialCharacters)
+        {
+            string password = "";
+
+            if (numbers && specialCharacters)
+            {
+
+            }
+            else if(specialCharacters && !numbers)
+            {
+
+            }
+            else if (!specialCharacters && numbers)
+            {
+
+            } else { 
+            }
+
+
+
+
+            return null; 
+        }
+
+        public static void updateDatabase()
+        {
+
+
+
+
+        }
+
+
+
+    }
+}
