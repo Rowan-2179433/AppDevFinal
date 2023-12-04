387180d0eb70a99a72b180b9912a18b30c28b6ed
Marc-Anthony Iacobellis <marcanthony307@yahoo.ca>
2023-12-03 22:33:40 -0500

    Added tabs to main form, serialization, and deserialization

--- PasswordProj/PasswordProj/PasswordGenerator.Designer.cs
+++ PasswordProj/PasswordProj/PasswordGenerator.Designer.cs
@@ -31,26 +31,38 @@
             this.lengthUpDown = new System.Windows.Forms.NumericUpDown();
             this.numbersCheck = new System.Windows.Forms.CheckBox();
             this.specialCheck = new System.Windows.Forms.CheckBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lengthLabel = new System.Windows.Forms.Label();
             this.copyButton = new System.Windows.Forms.Button();
             this.saveButton = new System.Windows.Forms.Button();
             this.outputTextBox = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.genLabel = new System.Windows.Forms.Label();
             this.generateButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.selectButton = new System.Windows.Forms.Button();
+            this.passwordControl = new System.Windows.Forms.TabControl();
+            this.generatorPage = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.readerSelectButton = new System.Windows.Forms.Button();
+            this.passwordTextBox = new System.Windows.Forms.RichTextBox();
+            this.revealButton = new System.Windows.Forms.Button();
+            this.displayPasswordsLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.lengthUpDown)).BeginInit();
+            this.passwordControl.SuspendLayout();
+            this.generatorPage.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // lengthUpDown
             // 
-            this.lengthUpDown.Location = new System.Drawing.Point(125, 111);
+            this.lengthUpDown.Location = new System.Drawing.Point(238, 100);
             this.lengthUpDown.Name = "lengthUpDown";
-            this.lengthUpDown.Size = new System.Drawing.Size(125, 20);
+            this.lengthUpDown.Size = new System.Drawing.Size(90, 20);
             this.lengthUpDown.TabIndex = 3;
             // 
             // numbersCheck
             // 
             this.numbersCheck.AutoSize = true;
-            this.numbersCheck.Location = new System.Drawing.Point(51, 56);
+            this.numbersCheck.Location = new System.Drawing.Point(140, 63);
             this.numbersCheck.Name = "numbersCheck";
             this.numbersCheck.Size = new System.Drawing.Size(68, 17);
             this.numbersCheck.TabIndex = 4;
@@ -60,37 +72,37 @@
             // specialCheck
             // 
             this.specialCheck.AutoSize = true;
-            this.specialCheck.Location = new System.Drawing.Point(125, 56);
+            this.specialCheck.Location = new System.Drawing.Point(214, 63);
             this.specialCheck.Name = "specialCheck";
             this.specialCheck.Size = new System.Drawing.Size(115, 17);
             this.specialCheck.TabIndex = 5;
             this.specialCheck.Text = "Special Characters";
             this.specialCheck.UseVisualStyleBackColor = true;
             // 
-            // label2
+            // lengthLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(27, 113);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(92, 13);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Password Length:";
+            this.lengthLabel.AutoSize = true;
+            this.lengthLabel.Location = new System.Drawing.Point(140, 102);
+            this.lengthLabel.Name = "lengthLabel";
+            this.lengthLabel.Size = new System.Drawing.Size(92, 13);
+            this.lengthLabel.TabIndex = 6;
+            this.lengthLabel.Text = "Password Length:";
             // 
             // copyButton
             // 
-            this.copyButton.Location = new System.Drawing.Point(33, 224);
+            this.copyButton.Location = new System.Drawing.Point(29, 259);
             this.copyButton.Name = "copyButton";
-            this.copyButton.Size = new System.Drawing.Size(86, 23);
+            this.copyButton.Size = new System.Drawing.Size(120, 46);
             this.copyButton.TabIndex = 7;
-            this.copyButton.Text = "Copy";
+            this.copyButton.Text = "Copy Password";
             this.copyButton.UseVisualStyleBackColor = true;
             this.copyButton.Click += new System.EventHandler(this.copyButton_Click);
             // 
             // saveButton
             // 
-            this.saveButton.Location = new System.Drawing.Point(159, 224);
+            this.saveButton.Location = new System.Drawing.Point(320, 259);
             this.saveButton.Name = "saveButton";
-            this.saveButton.Size = new System.Drawing.Size(77, 23);
+            this.saveButton.Size = new System.Drawing.Size(120, 46);
             this.saveButton.TabIndex = 8;
             this.saveButton.Text = "Save";
             this.saveButton.UseVisualStyleBackColor = true;
@@ -98,52 +110,156 @@
             // 
             // outputTextBox
             // 
-            this.outputTextBox.Location = new System.Drawing.Point(12, 188);
+            this.outputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputTextBox.Location = new System.Drawing.Point(29, 201);
+            this.outputTextBox.Multiline = true;
             this.outputTextBox.Name = "outputTextBox";
             this.outputTextBox.ReadOnly = true;
-            this.outputTextBox.Size = new System.Drawing.Size(242, 20);
+            this.outputTextBox.Size = new System.Drawing.Size(411, 27);
             this.outputTextBox.TabIndex = 10;
             this.outputTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // label1
+            // genLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(76, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(115, 24);
-            this.label1.TabIndex = 11;
-            this.label1.Text = "Generator: ";
+            this.genLabel.AutoSize = true;
+            this.genLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.genLabel.Location = new System.Drawing.Point(135, 19);
+            this.genLabel.Name = "genLabel";
+            this.genLabel.Size = new System.Drawing.Size(199, 24);
+            this.genLabel.TabIndex = 11;
+            this.genLabel.Text = "Password Generator";
             // 
             // generateButton
             // 
-            this.generateButton.Location = new System.Drawing.Point(61, 149);
+            this.generateButton.Location = new System.Drawing.Point(159, 142);
             this.generateButton.Name = "generateButton";
-            this.generateButton.Size = new System.Drawing.Size(155, 23);
+            this.generateButton.Size = new System.Drawing.Size(151, 28);
             this.generateButton.TabIndex = 12;
             this.generateButton.Text = "Generate";
             this.generateButton.UseVisualStyleBackColor = true;
             this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
             // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(29, 327);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(411, 59);
+            this.exitButton.TabIndex = 13;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // selectButton
+            // 
+            this.selectButton.Location = new System.Drawing.Point(174, 259);
+            this.selectButton.Name = "selectButton";
+            this.selectButton.Size = new System.Drawing.Size(120, 46);
+            this.selectButton.TabIndex = 14;
+            this.selectButton.Text = "Select File";
+            this.selectButton.UseVisualStyleBackColor = true;
+            this.selectButton.Click += new System.EventHandler(this.selectButton_Click);
+            // 
+            // passwordControl
+            // 
+            this.passwordControl.Controls.Add(this.generatorPage);
+            this.passwordControl.Controls.Add(this.tabPage2);
+            this.passwordControl.Location = new System.Drawing.Point(12, 9);
+            this.passwordControl.Name = "passwordControl";
+            this.passwordControl.SelectedIndex = 0;
+            this.passwordControl.Size = new System.Drawing.Size(476, 454);
+            this.passwordControl.TabIndex = 15;
+            // 
+            // generatorPage
+            // 
+            this.generatorPage.Controls.Add(this.saveButton);
+            this.generatorPage.Controls.Add(this.selectButton);
+            this.generatorPage.Controls.Add(this.lengthUpDown);
+            this.generatorPage.Controls.Add(this.exitButton);
+            this.generatorPage.Controls.Add(this.numbersCheck);
+            this.generatorPage.Controls.Add(this.generateButton);
+            this.generatorPage.Controls.Add(this.specialCheck);
+            this.generatorPage.Controls.Add(this.genLabel);
+            this.generatorPage.Controls.Add(this.lengthLabel);
+            this.generatorPage.Controls.Add(this.outputTextBox);
+            this.generatorPage.Controls.Add(this.copyButton);
+            this.generatorPage.Location = new System.Drawing.Point(4, 22);
+            this.generatorPage.Name = "generatorPage";
+            this.generatorPage.Padding = new System.Windows.Forms.Padding(3);
+            this.generatorPage.Size = new System.Drawing.Size(468, 428);
+            this.generatorPage.TabIndex = 0;
+            this.generatorPage.Text = "Generator";
+            this.generatorPage.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.readerSelectButton);
+            this.tabPage2.Controls.Add(this.passwordTextBox);
+            this.tabPage2.Controls.Add(this.revealButton);
+            this.tabPage2.Controls.Add(this.displayPasswordsLabel);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(468, 428);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Reader";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // readerSelectButton
+            // 
+            this.readerSelectButton.Location = new System.Drawing.Point(147, 49);
+            this.readerSelectButton.Name = "readerSelectButton";
+            this.readerSelectButton.Size = new System.Drawing.Size(86, 23);
+            this.readerSelectButton.TabIndex = 19;
+            this.readerSelectButton.Text = "Select File";
+            this.readerSelectButton.UseVisualStyleBackColor = true;
+            this.readerSelectButton.Click += new System.EventHandler(this.readerSelectButton_Click);
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(18, 87);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.ReadOnly = true;
+            this.passwordTextBox.Size = new System.Drawing.Size(433, 327);
+            this.passwordTextBox.TabIndex = 18;
+            this.passwordTextBox.Text = "";
+            // 
+            // revealButton
+            // 
+            this.revealButton.Location = new System.Drawing.Point(247, 49);
+            this.revealButton.Name = "revealButton";
+            this.revealButton.Size = new System.Drawing.Size(75, 23);
+            this.revealButton.TabIndex = 17;
+            this.revealButton.Text = "Reveal Passwords";
+            this.revealButton.UseVisualStyleBackColor = true;
+            this.revealButton.Click += new System.EventHandler(this.revealButton_Click);
+            // 
+            // displayPasswordsLabel
+            // 
+            this.displayPasswordsLabel.AutoSize = true;
+            this.displayPasswordsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayPasswordsLabel.Location = new System.Drawing.Point(143, 12);
+            this.displayPasswordsLabel.Name = "displayPasswordsLabel";
+            this.displayPasswordsLabel.Size = new System.Drawing.Size(183, 24);
+            this.displayPasswordsLabel.TabIndex = 16;
+            this.displayPasswordsLabel.Text = "Display Passwords";
+            // 
             // PasswordGenerator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(266, 271);
-            this.Controls.Add(this.generateButton);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.outputTextBox);
-            this.Controls.Add(this.saveButton);
-            this.Controls.Add(this.copyButton);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.specialCheck);
-            this.Controls.Add(this.numbersCheck);
-            this.Controls.Add(this.lengthUpDown);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(500, 472);
+            this.Controls.Add(this.passwordControl);
+            this.MaximizeBox = false;
             this.Name = "PasswordGenerator";
-            this.Text = "Password";
+            this.Text = "The Vault - Password Generator";
             ((System.ComponentModel.ISupportInitialize)(this.lengthUpDown)).EndInit();
+            this.passwordControl.ResumeLayout(false);
+            this.generatorPage.ResumeLayout(false);
+            this.generatorPage.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -151,12 +267,21 @@
         private System.Windows.Forms.NumericUpDown lengthUpDown;
         private System.Windows.Forms.CheckBox numbersCheck;
         private System.Windows.Forms.CheckBox specialCheck;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lengthLabel;
         private System.Windows.Forms.Button copyButton;
         private System.Windows.Forms.Button saveButton;
         private System.Windows.Forms.TextBox outputTextBox;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label genLabel;
         private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button selectButton;
+        private System.Windows.Forms.TabControl passwordControl;
+        private System.Windows.Forms.TabPage generatorPage;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button readerSelectButton;
+        private System.Windows.Forms.RichTextBox passwordTextBox;
+        private System.Windows.Forms.Button revealButton;
+        private System.Windows.Forms.Label displayPasswordsLabel;
     }
 }
 
--- PasswordProj/PasswordProj/PasswordGenerator.cs
+++ PasswordProj/PasswordProj/PasswordGenerator.cs
@@ -1,14 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Windows.Forms;
 
 
 namespace PasswordProj
@@ -19,10 +14,18 @@ namespace PasswordProj
     //Implement file saving 
     public partial class PasswordGenerator : Form
     {
+        private BinaryFormatter formatter = new BinaryFormatter();
+        private FileStream output;
+        private string selectedFilePath;
+        private List<string> hiddenPasswords = new List<string>(); // To store actual passwords
+        private bool isPasswordVisible = false; // Flag to track visibility state
+        private int counter = 0;
 
         public PasswordGenerator()
         {
             InitializeComponent();
+            passwordTextBox.Font = new Font(passwordTextBox.Font.FontFamily, 14);
+
         }
         //Helper method to save the password into a file
         public static void SavePasswordToFile(string password, string filePath)
@@ -36,13 +39,32 @@ namespace PasswordProj
         //Saves generated password and prompts the user with the save form 
         private void saveButton_Click(object sender, EventArgs e)
         {
-            string password = outputTextBox.Text;
-            string filePath = Path.Combine(Environment.CurrentDirectory, "Database.ser");
+            try
+            {
+                string password = outputTextBox.Text;
+                if (string.IsNullOrWhiteSpace(password))
+                {
+                    MessageBox.Show("Please enter a password before saving.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return; // Exit the method to prevent saving an empty password
+                }
 
-            PasswordGenerator.SavePasswordToFile(password, filePath);
+                // Check if an output stream has been selected
+                if (output == null)
+                {
+                    MessageBox.Show("Please select an output file first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return; // Exit the method if no output stream is selected
+                }
 
-            // Optionally, show a message to the user that the file has been saved
-            MessageBox.Show("Password saved successfully.");
+                // Serialize and write the password to the output stream
+                formatter.Serialize(output, password);
+
+                // Show a message to the user that the file has been saved
+                MessageBox.Show("Password saved successfully.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while saving the password to the file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
 
@@ -50,7 +72,7 @@ namespace PasswordProj
         private void generateButton_Click(object sender, EventArgs e)
         {
             Generators gen = new Generators();
-            
+
             bool numbers = false;
             bool special = false;
 
@@ -69,15 +91,122 @@ namespace PasswordProj
 
             string password = gen.generatePassword(length, numbers, special);
 
-            outputTextBox.Text = password; 
+            outputTextBox.Text = password;
 
         }
 
         //Status: Complete 
         private void copyButton_Click(object sender, EventArgs e)
         {
-            Clipboard.SetText(outputTextBox.Text);
+            try
+            {
+                Clipboard.SetText(outputTextBox.Text);
+                MessageBox.Show("Text copied to clipboard successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while copying the password to the clipboard: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
 
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                output?.Close();
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Cannot close file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            Application.Exit();
+        }
+
+        private void selectButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog fileChooser = new OpenFileDialog())
+            {
+
+                DialogResult result = fileChooser.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    string selectedFileName = fileChooser.FileName;
+
+                    if (string.IsNullOrEmpty(selectedFileName))
+                    {
+                        MessageBox.Show("Invalid File Name", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        saveButton.Enabled = false; // Disable save button if the file name is empty
+                    }
+                    else
+                    {
+                        try
+                        {
+                            output = new FileStream(selectedFileName, FileMode.Append, FileAccess.Write);
+                            saveButton.Enabled = true; // Enable save button when a valid file is selected
+                        }
+                        catch (IOException)
+                        {
+                            MessageBox.Show("Error opening file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            saveButton.Enabled = false; // Disable save button on error
+                        }
+                    }
+                }
+                else
+                {
+                    saveButton.Enabled = false; // Disable save button if the user cancels file selection
+                }
+            }
+
+        }
+
+        private void readerSelectButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog fileChooser = new OpenFileDialog())
+            {
+                if (fileChooser.ShowDialog() == DialogResult.OK)
+                {
+                    selectedFilePath = fileChooser.FileName;
+                    ReadAndDisplayPasswords(selectedFilePath, false);
+                }
+            }
+        }
+
+        private void revealButton_Click(object sender, EventArgs e)
+        {
+            isPasswordVisible = !isPasswordVisible; // Toggle visibility
+            ReadAndDisplayPasswords(selectedFilePath, isPasswordVisible);
+        }
+        private void ReadAndDisplayPasswords(string filePath, bool showPasswords)
+        {
+            try
+            {
+                using (FileStream stream = new FileStream(filePath, FileMode.Open))
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    hiddenPasswords.Clear(); // Clear the list for new data
+                    passwordTextBox.Clear(); // Clear the RichTextBox for new data
+                    counter = 0;
+
+                    while (stream.Position < stream.Length)
+                    {
+                        counter++;
+                        string password = (string)formatter.Deserialize(stream);
+                        hiddenPasswords.Add(password);
+
+                        string displayText = showPasswords ? password : new string('*', password.Length);
+                        passwordTextBox.AppendText("Password #" + counter + ": " + displayText + Environment.NewLine + Environment.NewLine);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while reading the file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
+   
+
+
